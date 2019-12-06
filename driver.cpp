6048cefb8e0c6042a5854a2901c6ae362825aa61
jaiveerbhaidasna <43055551+jaiveerbhaidasna@users.noreply.github.com>
2019-12-06 14:50:35 -0600

    Finished product

--- driver.cpp
+++ driver.cpp
@@ -5,10 +5,11 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
-/*function... might want it in some class?*/
+//Function that gets all files from the given directory and adds them to a file vector to be used later
 int getdir (string dir, vector<string> &files)
 {
     DIR *dp;
@@ -25,12 +26,14 @@ int getdir (string dir, vector<string> &files)
     return 0;
 }
 
+//Takes a file from the file vector and adds all of the words from that file to a different vector for future usage
 void getAllWords(string fileName, vector<string> &words, string path)
 {
     string pathName = path + "/" + fileName;
     ifstream inFile;
-    inFile.open(pathName);
     string word;
+    inFile.open(pathName);
+
 
     inFile >> word;
     while(inFile)
@@ -42,6 +45,8 @@ void getAllWords(string fileName, vector<string> &words, string path)
     inFile.close();
 }
 
+//Helper function that fixes all 'chunks' of string so that only valid characters are considered and also makes all
+//letters lowercase
 string fixString(string s)
 {
     string value;
@@ -54,12 +59,17 @@ string fixString(string s)
                 s[i]+=32;
                 value+=s[i];
             }
+            else
+                if(s[i] == '<')
+                    i+=2;
     }
 
     return value;
 }
 
-void processChunks(vector<string> words, int n)
+//Takes a given set of words from a file and concatenates them into chunks of n size based on user input before storing
+//them in
+void processChunks(vector<string> words, int n, vector<string> &chunks)
 {
     string chunk;
     for(int i = 0; i < words.size() - n + 1; i++)
@@ -70,25 +80,126 @@ void processChunks(vector<string> words, int n)
             chunk+= words[y];
         }
         chunk = fixString(chunk);
-        cout << chunk << "\n";
+        chunks.push_back(chunk);
     }
 }
 
-int main()
+//Helper function that takes two integers and takes returns the value of one raised to the other
+int power(int base, int exp)
 {
-    string dir = string("sm_doc_set");
-    vector<string> files = vector<string>();
-    vector<string> words = vector<string>();
-    int chunkSize = 6;
+    if(exp == 0)
+        return 1;
+    else
+        if(exp == 1)
+            return base;
+        else
+            return base * power(base,exp-1);
+}
+
+//Hashing function that takes all chunks of a certain file and hashes them into the hash table
+void hashValues(vector<string> chunks, vector<vector<int>> &hashTable, int tableSize, int fileNumber)
+{
+    unsigned int hashValue;
+    bool contained;
+    string chunk;
+
+    for(int i = 0; i < chunks.size(); i++)
+    {
+        contained = false;
+        hashValue = 0;
+        chunk = chunks[i];
+        for(int j = 0; j < chunk.length(); j++)
+        {
+            hashValue += (int)(chunk[chunk.length()-j-1]) * power(11,j);
+        }
+        if(hashValue > tableSize)
+            hashValue = hashValue % tableSize;
+
+        for(int k = 0; k < hashTable[hashValue].size(); k++)
+            if(hashTable[hashValue][k] == fileNumber)
+                contained = true;
+        if(!contained)
+            hashTable[hashValue].push_back(fileNumber);
+    }
+}
+
+//Checks the hash table and records collisions of files in a matrix
+void checkCollisions(vector<vector<int>> &collisions, vector<vector<int>> &hashTable, int hashSize)
+{
+    for(int i = 0; i < hashSize; i++)
+    {
+        if(!hashTable[i].empty())
+        {
+            for (int j = 0; j < hashTable[i].size() - 1; j++)
+                for (int k = j + 1; k < hashTable[i].size(); k++) {
+                    collisions[hashTable[i][j]][hashTable[i][k]]++;
+                }
+        }
+    }
+}
+
+//Checks the collisions matrix and outputs the file pairs that show high amounts of plagiarism
+void outputResults(vector<vector<int>> &collisions, int numFiles, int plagiarismCutoff, vector<string> &files)
+{
+    int counter = 0;
+    int max;
+    int maxR, maxC;
+
+    for(int i = 0; i < numFiles; i++)
+    {
+        for (int j = 0; j < numFiles; j++)
+        {
+            if (collisions[i][j] >= plagiarismCutoff){
+                counter++;
+            }
+        }
+    }
+
+    while(counter > 0)
+    {
+        max = 0;
+        for(int r = 0; r < numFiles; r++)
+            for(int c = 0; c < numFiles; c++)
+                if(collisions[r][c] > max)
+                {
+                    max = collisions[r][c];
+                    maxR = r;
+                    maxC = c;
+                }
+        cout << max << ": " << files[maxR] << ", " << files[maxC] << endl;
+        collisions[maxR][maxC] = 0;
+        counter--;
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    const unsigned int HASH_SIZE = 3000017;                     //Constant size or hash table
+    string dir = string(argv[1]);
+    vector<string> files;
+    vector<string> words;
+    vector<string> chunked;
+    vector<vector<int>> hashTable(HASH_SIZE,vector<int>(0));
+    int chunkSize = atoi(argv[2]);
+    int plagSize = atoi(argv[3]);
 
     getdir(dir,files);
-/*
-    for (int i = 2;i < files.size();i++) {
+
+    files.erase(files.begin());                                 //First two files are unnecessary
+    files.erase(files.begin());
+    vector<vector<int>> collisions(files.size(),vector<int>(files.size(),0));
+
+    for (int i = 0; i < files.size();i++) {                     //Loop that hashes every file from the directory
         words.clear();
+        chunked.clear();
         getAllWords(files[i],words,dir);
-        processChunks(words,chunkSize);
-    } */
-    getAllWords(files[2],words,dir);
-    processChunks(words,chunkSize);
+        processChunks(words,chunkSize,chunked);
+        hashValues(chunked,hashTable,HASH_SIZE,i);
+    }
+
+    checkCollisions(collisions,hashTable,HASH_SIZE);            //Check the resulting collisions and display
+    outputResults(collisions,files.size(),plagSize,files);
+
     return 0;
 }
